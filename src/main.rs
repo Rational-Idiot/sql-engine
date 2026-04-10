2b22d5e1505e4b9403790d90bfd67b0f841a87e7
Aryan <ether9095cable@gmail.com>
2026-04-10 07:53:10 +0530

    Fixing up

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,7 @@ use db::{
 };
 
 fn main() {
-    let query = "
-        SELECT u.id, u.name, u.score
-        FROM users u
-        WHERE 
-            u.score BETWEEN 10 AND 100
-            AND u.id IN (SELECT id FROM active_users)
-            AND EXISTS (
-                SELECT * FROM logs l 
-                WHERE l.user_id = u.id AND l.action LIKE 'login%'
-            )
-            OR u.name ILIKE 'A%'
-        ORDER BY u.score DESC, u.name ASC
-        LIMIT 10 OFFSET 5;
-    ";
+    let query = " SELECT u.id, u.name, u.score FROM users u WHERE u.score BETWEEN 10 AND 100 AND u.id IN (SELECT id FROM active_users) AND EXISTS ( SELECT * FROM logs l WHERE l.user_id = u.id AND l.action LIKE 'login%') OR u.name ILIKE 'A%' ORDER BY u.score DESC, u.name ASC LIMIT 10 OFFSET 5; ";
 
     let mut lexer = Lex::new();
     lexer.input = query.chars().collect();
